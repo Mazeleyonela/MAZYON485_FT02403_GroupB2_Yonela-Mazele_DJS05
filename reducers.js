392756cbf8470f0f0259d0c471ecfb30882e0f17
Yonela Mazele <mazeleyonela8@gmail.com>
2024-07-31 18:01:16 +0200

    created a reducer to manage the tally counter state

---
+++ reducers.js
@@ -0,0 +1,16 @@
+// Reducers specify how the application's state changes in response to actions sent to the store.
+
+const initialState = { count: 0 }; // this the first scenario, which is the Initial State Verification
+
+function tallyReducer(state = initialState, action) {
+    switch (action.type) {
+        case 'ADD':
+            return { count: state.count + 1 }; // incrementing the counter 
+        case 'SUBTRACT':
+            return { count: state.count - 1 };  // decrementing the counter 
+        case 'RESET':
+            return { count: 0 };  // Resetting the Counter
+        default:
+            return state;
+    }
+}
